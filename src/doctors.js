ce61667980e9f8121328503ee04dccd9c5a9e358
Erin Deuel <end.gaia@gmail.com>
2019-09-13 11:23:22 -0700

    start to get things to work... sort of

--- src/doctors.js
+++ src/doctors.js
@@ -1,20 +1,17 @@
-import $ from 'jquery';
-import './styles.css';
-import 'bootstrap';
-
 export class DoctorSearch{
-  searchingData(){
-        let request = new XMLHttpRequest();
-        const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name='+name+'&query='+symptom+'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=9e8a57517279067adb27f63a27c532bc';
-        request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(error(request.statusText));
-        }
-      }
-      request.open('GET', url, true);
-      request.send();
-    });
+  searchingData(name, issue){
+    let request = new XMLHttpRequest();
+    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name='+ name +'&query='+ issue +'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=9e8a57517279067adb27f63a27c532bc';
+    request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+    request.open("GET", url, true);
+    request.send();
+    const getElements = function(response) {
+     $('#list').append(`<li>'${response.doctor.profile.bio}'</li>`);
+   };
+  };
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import { DoctorSearch } from './doctors.js';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import 'bootstrap';
 
-(document).ready(function(){
-  ('.search').click(function(event){
-    event.preventDefault();
+$(document).ready(function(){
+  $('.search').submit(function(event){
     let issue = $("#medicalProblem").val();
     let name = $("#doctorName").val();
+    event.preventDefault();
     $("#medicalProblem").val("");
     $('#doctorName').val("");
+    console.log(name);
+    console.log(issue);
 
     let newSearch = new DoctorSearch();
     let userSearch = newSearch.searchingData(name, issue);
