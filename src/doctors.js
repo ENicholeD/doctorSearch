15a663a26dea1991b4103a82c6b82e50fdd9ed5d
Erin Deuel <end.gaia@gmail.com>
2019-09-13 10:52:51 -0700

    try again

--- src/doctors.js
+++ src/doctors.js
@@ -1,10 +1,20 @@
-import 'jquery';
+import $ from 'jquery';
 import './styles.css';
 import 'bootstrap';
 
 export class DoctorSearch{
   searchingData(){
         let request = new XMLHttpRequest();
-        const url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}';
+        const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name='+name+'&query='+symptom+'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=9e8a57517279067adb27f63a27c532bc';
+        request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(error(request.statusText));
+        }
+      }
+      request.open('GET', url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,21 +3,12 @@ import { DoctorSearch } from './doctors.js';
 (document).ready(function(){
   ('.search').click(function(event){
     event.preventDefault();
+    let issue = $("#medicalProblem").val();
+    let name = $("#doctorName").val();
+    $("#medicalProblem").val("");
+    $('#doctorName').val("");
+
     let newSearch = new DoctorSearch();
-    let issue = $("#medicalIssue").val();
-    let name = $("doctorName").val();
-
-    let request = new XMLHttpRequest();
-    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}';
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
+    let userSearch = newSearch.searchingData(name, issue);
   });
 });
